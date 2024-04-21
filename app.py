4348d7a94b7dadf73db2c4768e7b503758738174
ofek barel <fikfok67@gmail.com>
2024-04-21 14:10:42 +0300

    add app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello():
+    return render_template('cat.html')
+
+
+
+
+@app.route("/")
+def hello():
+    return "Example app"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
